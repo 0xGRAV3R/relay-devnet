aeac3bb8adc0d3f3336301dc594884f0e4467fdb
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-07-01 02:06:48 -0700

    🚨 Fixing the modal

--- web/components/relay/relay-feature.tsx
+++ web/components/relay/relay-feature.tsx
@@ -26,6 +26,24 @@ export default function RelayFeature() {
 
 
       {/* Added Modal */} 
+      {/* The button to open modal */}
+<label htmlFor="my_modal_7" className="btn">open modal</label>
+
+{/* Put this part before </body> tag */}
+<input type="checkbox" id="my_modal_7" className="modal-toggle" />
+<div className="modal" role="dialog">
+  <div className="modal-box">
+    <h3 className="text-lg font-bold">Hello!</h3>
+    <p className="py-4">This modal works with a hidden checkbox!</p>
+  </div>
+  <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
+</div>
+
+
+
+
+
+
       <div className="drawer">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
