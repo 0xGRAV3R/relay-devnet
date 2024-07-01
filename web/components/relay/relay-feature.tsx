4d3732c626341ceb8385476650d5f336bf22a569
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-07-01 01:19:19 -0700

    🚨 Fixing error with modal

--- web/components/relay/relay-feature.tsx
+++ web/components/relay/relay-feature.tsx
@@ -15,23 +15,23 @@ export default function RelayFeature() {
     <div>
       
       <AppHero
-        /*
+        
         title="Relay - Sol Messenger"
         subtitle={
           'Send your message here!'
         }
-        */
+        
       >
       
+      {/* The button to open modal */}
+      <label htmlFor="my_modal_7" className="btn">✍️ Send Message</label>
 
-      {/* Added Modal */} 
-      {/* Open the modal using document.getElementById('ID').showModal() method */}
-      <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>✍️ Send Message</button>
-      <dialog id="my_modal_1" className="modal">
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+      <div className="modal" role="dialog">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Relay - Sol Messenger</h3>
-          <p className="py-4">Send your message here! <br /> Press ESC key or click the button below to close</p>
-          
+          <h3 className="text-lg font-bold">Relay - Sol Messenger</h3>
+          <p className="py-4">Send your message here! <br /> Press ESC key to close</p>
           <p className="mb-6">
           <ExplorerLink
             path={`account/${programId}`}
@@ -39,14 +39,26 @@ export default function RelayFeature() {
           />
           </p>
           <RelayCreate />
-          <div className="modal-action">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
-            </form>
-          </div>
         </div>
-      </dialog>
+        <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
+      </div>
+
+
+      {/* Added Modal */} 
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+<button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>open modal</button>
+<dialog id="my_modal_1" className="modal">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">Hello!</h3>
+    <p className="py-4">Press ESC key or click the button below to close</p>
+    <div className="modal-action">
+      <form method="dialog">
+        {/* if there is a button in form, it will close the modal */}
+        <button className="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
       {/* Added Modal */} 
 
       </AppHero>
