f566ecb853f804a0d04d4b287fed7440ec4df26d
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-07-04 15:06:15 -0700

    🔘 Making close account button smaller

--- web/components/relay/relay-ui.tsx
+++ web/components/relay/relay-ui.tsx
@@ -169,10 +169,10 @@ function RelayCard({ account }: { account: PublicKey }) {
                 path={`account/${account}`}
                 label={ellipsify(account.toString())}
               />
-
+            &nbsp;
             <button
               /*className="btn btn-xs btn-error btn-outline"*/
-              className="btn btn-xs"
+              className="btn btn-circle btn-xs "
               onClick={() => {
                 if (
                   !window.confirm(
@@ -188,7 +188,19 @@ function RelayCard({ account }: { account: PublicKey }) {
               }}
               disabled={deleteEntry.isPending}
             >
-              🚫
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12" />
+              </svg>
+              
             </button>
             </p>
             </div>
