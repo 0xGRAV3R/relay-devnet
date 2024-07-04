08b9be78f40bf6e04928b07867d304430880ecb6
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-07-04 15:42:10 -0700

    🔘 Making close account button as a circle

--- web/components/relay/relay-ui.tsx
+++ web/components/relay/relay-ui.tsx
@@ -190,7 +190,7 @@ function RelayCard({ account }: { account: PublicKey }) {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4"
+                className="h-3 w-3"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor">
